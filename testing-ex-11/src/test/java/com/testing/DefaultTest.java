3c65a4b76f2aa9a817e1c9f8a297192675ae85ef
Sorin Dan <dansorin2006@gmail.com>
2021-12-22 17:41:34 +0200

    fix(master): Updated to use blog for test and download chromedriver from Internet on testing-ex-11

--- testing-ex-11/src/test/java/com/testing/DefaultTest.java
+++ testing-ex-11/src/test/java/com/testing/DefaultTest.java
@@ -1,21 +1,29 @@
 package com.testing;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static org.testng.Assert.assertTrue;
 
 public class DefaultTest {
 
     @Test
     public void defaultTest() {
-        open("https://www.google.com");
-        $(By.xpath("//input[@name='q']")).setValue("selenium webdriver");
-        $(By.xpath("//input[@name='q']")).submit();
-        $(By.xpath("//h3[@class='LC20lb']")).shouldBe(Condition.visible);
+        Configuration.browser = "chrome";
+
+        open("https://t3ch5tuff5.wordpress.com");
+
+        $$(By.xpath("//ul[@id='menu-primary-1']/li")).get(1).click();
+        $(By.className("entry-title")).shouldBe(Condition.visible).shouldHave(Condition.text("Blog"));
+
+        $$(By.xpath("//ul[@id='menu-primary-1']/li")).get(2).click();
+        $(By.className("entry-title")).shouldBe(Condition.visible).shouldHave(Condition.text("About"));
+
+        $$(By.xpath("//ul[@id='menu-primary-1']/li")).get(3).click();
+        $(By.className("entry-title")).shouldBe(Condition.visible).shouldHave(Condition.text("Contact"));
     }
 
 }
